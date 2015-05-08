66e77345807b75af8dce0c92623b0802f3604e3f
kohsuke-sato <kohsuke.sato.1202@gmail.com>
2015-05-08 22:07:01 +0800

    Create mechanicalTurk.c
    This is the file that will be submitted to "Mechanical Turk"

---
+++ mechanicalTurk.c
@@ -0,0 +1,11 @@
+// mechanicalTurk.c
+// Created by Albert Smith and Kohsuke Sato
+// A file submission to "Mechanical Turk"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char* argv[]) {
+    // Enter code here...
+    return EXIT_SUCCESS;
+}
