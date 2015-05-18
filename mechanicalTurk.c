eff764031c7c71d77db31d653ce3a509f8d6f8fa
kohsuke-sato <kohsuke.sato.1202@gmail.com>
2015-05-19 06:40:47 +0800

    Follow the style guide, fix the errors and add Mr GO8

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -10,16 +10,14 @@
 #include "mechanicalTurk.h"
 
 #define ALL_PATHS {"RRLR", "RRLRL", "RRLRLL", "RRLRLLR", "RRLRLLRL",\
- "RRLRLLRLR", "RRLRLLRLRL", "RR", "RRL", "RRLL", "RRLLR", "RRLLRL", "RRLLRLR",\
- "RRLLRLRL", "RRLLRLRLR", "RRLLRLRLRL", "", "R", "RL", "RLR", "RLRL", "RLRLR",\
- "RLRLRL", "RLRLRLR", "RLRLRLRL", "RLRLRLRLR", "RLRLRLRLRL", "L", "LR", "LRR",\
- "LRRL", "LRRLR", "LRRLRL", "LRRLRLR", "LRRLRLRL", "LRRLRLRLR", "LRRLRLRLRL",\
- "LRRLRLRLRLR", "LRL", "LRLR", "LRLRR", "LRLRRL", "LRLRRLR", "LRLRRLRL",\
- "LRLRRLRLR", "LRLRRLRLRL", "LRLRRLRLRLR", "LRLRL", "LRLRLR", "LRLRLRR",\
- "LRLRLRRL", "LRLRLRRLR", "LRLRLRRLRL", "LRLRLRRLRLR"}
- 
-#define TRUE 1
-#define FALSE !TRUE
+"RRLRLLRLR", "RRLRLLRLRL", "RR", "RRL", "RRLL", "RRLLR", "RRLLRL", \
+"RRLLRLR", "RRLLRLRL", "RRLLRLRLR", "RRLLRLRLRL", "", "R", "RL", "RLR",\
+"RLRL", "RLRLR", "RLRLRL", "RLRLRLR", "RLRLRLRL", "RLRLRLRLR", \
+"RLRLRLRLRL", "L", "LR", "LRR", "LRRL", "LRRLR", "LRRLRL", "LRRLRLR", \
+"LRRLRLRL", "LRRLRLRLR", "LRRLRLRLRL", "LRRLRLRLRLR", "LRL", "LRLR", \
+"LRLRR", "LRLRRL", "LRLRRLR", "LRLRRLRL","LRLRRLRLR", "LRLRRLRLRL", \
+"LRLRRLRLRLR", "LRLRL", "LRLRLR", "LRLRLRR","LRLRLRRL", "LRLRLRRLR", \
+"LRLRLRRLRL", "LRLRLRRLRLR"}
 
 action decideAction (Game g) {
     action nextAction;
@@ -44,10 +42,10 @@ action decideAction (Game g) {
         while ((i < sizeof(paths) / sizeof(paths[0])) && !chosen)
         {
             testAction.actionCode = BUILD_CAMPUS;
-            testAction.destination = paths[i];
+            testAction.destination = arcs[i];
             if (isLegalAction(g, testAction)) {
                 nextAction.actionCode = BUILD_CAMPUS;
-                nextAction.destination = paths[i];
+                nextAction.destination = arcs[i];
                 chosen = TRUE;
             }
             i++;
@@ -64,6 +62,22 @@ action decideAction (Game g) {
             testAction.destination = paths[i];
             if (isLegalAction(g, testAction)) {
                 nextAction.actionCode = BUILD_ARC;
+                nextAction.destination = paths[i];
+                chosen = TRUE;
+            }
+            i++;
+        }
+    }
+    
+    // Mr GO8
+    if (!chosen) {
+        path paths[] = ALL_PATHS;
+        int i = 0;
+        while ((i < sizeof(paths) / sizeof(paths[0])) & !chosen) {
+            testAction.actionCode = BUILD_GO8;
+            testAction.destination = paths[i];
+            if (isLegalAction(g, testAction)) {
+                nextAction.actionCode = BUILD_GO8;
                 nextAction.destination = paths[i];
                 chosen = TRUE;
             }
