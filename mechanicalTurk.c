38b0f293c8767e10e8883418ecbe1acef01442e4
TheBrainyOne <albert.chejin.edward.smith@outlook.com>
2015-05-18 20:44:44 +0800

    Make code more workable

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -9,22 +9,66 @@
 #include "Game.h"
 #include "mechanicalTurk.h"
 
+#define ALL_PATHS {"RRLR", "RRLRL", "RRLRLL", "RRLRLLR", "RRLRLLRL",\
+ "RRLRLLRLR", "RRLRLLRLRL", "RR", "RRL", "RRLL", "RRLLR", "RRLLRL", "RRLLRLR",\
+ "RRLLRLRL", "RRLLRLRLR", "RRLLRLRLRL", "", "R", "RL", "RLR", "RLRL", "RLRLR",\
+ "RLRLRL", "RLRLRLR", "RLRLRLRL", "RLRLRLRLR", "RLRLRLRLRL", "L", "LR", "LRR",\
+ "LRRL", "LRRLR", "LRRLRL", "LRRLRLR", "LRRLRLRL", "LRRLRLRLR", "LRRLRLRLRL",\
+ "LRRLRLRLRLR", "LRL", "LRLR", "LRLRR", "LRLRRL", "LRLRRLR", "LRLRRLRL",\
+ "LRLRRLRLR", "LRLRRLRLRL", "LRLRRLRLRLR", "LRLRL", "LRLRLR", "LRLRLRR",\
+ "LRLRLRRL", "LRLRLRRLR", "LRLRLRRLRL", "LRLRLRRLRLR"}
+ 
+#define TRUE 1
+#define FALSE !TRUE
+
 action decideAction (Game g) {
     action nextAction;
     action testAction;
     
-    // Mr Pass
+    char chosen = FALSE;
+    
+    // Mr Pass - commented out for now
+    /*
     testAction.actionCode = START_SPINOFF;
     if (isLegalAction(g, testAction)) {
         nextAction.actionCode = START_SPINOFF;
     } else {
         nextAction.actionCode = PASS;
     }
-    
+    */
+
     // Mr Campus
-    testAction.actionCode = BUILD_CAMPUS;
-    if (isLegalAction(g, testAction)) {
-        nextAction.actionCode = BUILD_CAMPUS;
+    if (!chosen) {
+        path paths[] = ALL_PATHS;
+        int i = 0;
+        while ((i < sizeof(paths) / sizeof(paths[0])) && !chosen)
+        {
+            testAction.actionCode = BUILD_CAMPUS;
+            testAction.destination = paths[i];
+            if (isLegalAction(g, testAction)) {
+                nextAction.actionCode = BUILD_CAMPUS;
+                nextAction.destination = paths[i];
+                chosen = TRUE;
+            }
+            i++;
+        }
+    }
+    
+    // The roots of Mr ARC - very buggy at the moment
+    if (!chosen) {
+        path arcs[] = ALL_PATHS;
+        int i = 0;
+        while ((i < sizeof(arcs) / sizeof(arcs[0])) && !chosen)
+        {
+            testAction.actionCode = BUILD_ARC;
+            testAction.destination = paths[i];
+            if (isLegalAction(g, testAction)) {
+                nextAction.actionCode = BUILD_ARC;
+                nextAction.destination = paths[i];
+                chosen = TRUE;
+            }
+            i++;
+        }
     }
     
     return nextAction;
